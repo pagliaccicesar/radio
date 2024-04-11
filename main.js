f2a7b46b45334560823f343ec5ac6da29301f26a
cesar pagliacci <pagliaccicesar@gmail.com>
2024-04-11 20:57:50 -0300

    mi primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('#show').on('click',function(){        
+        $('.card-reveal').slideToggle('slow');
+    });
+    
+    $('.card-reveal .close').on('click',function(){
+        $('.card-reveal').slideToggle('slow');
+    });
+});
+
